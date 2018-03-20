d8a9b306105996d9def58f36767770aac4cab93f
Isitha Subasinghe <isubasinghe@users.noreply.github.com>
2018-03-20 11:33:56 +1100

    Initial commit

---
+++ query.c
@@ -0,0 +1,16 @@
+/* ... */
+
+#include <stdio.h>
+
+#include "query.h"
+#include "list.h"
+
+// see Assignment spec: 'Task 1: Array-based accumulation approach'
+void print_array_results(Index *index, int n_results, int n_documents) {
+	printf("not yet implemented: put code for task 1 here\n");
+}
+
+// see Assignment spec: 'Task 2: Priority queue-based multi-way merge approach'
+void print_merge_results(Index *index, int n_results) {
+	printf("not yet implemented: put code for task 2 here\n");
+}
