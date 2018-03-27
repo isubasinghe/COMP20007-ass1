8244f0e0121ee08d861bdfdea2bf8320ec4c062d
Isitha Subasinghe <isubasinghe@users.noreply.github.com>
2018-03-27 18:31:46 +1100

    commented

--- query.c
+++ query.c
@@ -10,9 +10,11 @@
 // see Assignment spec: 'Task 1: Array-based accumulation approach'
 void print_array_results(Index *index, int n_results, int n_documents) {
 	
+	// Allocate n_documents scores
 	float *scores = malloc(n_documents*sizeof(float));
 
 	for(int i=0; i < n_documents; i++) {
+		// We need to assure that all the elems are 0.
 		scores[i] = 0.0;
 	} // O(d)
 
@@ -23,29 +25,42 @@ void print_array_results(Index *index, int n_results, int n_documents) {
 		// then do a scores[id] += score;
 		List *list = index->doclists[i];
 
+		//Obtain the first node of the list.
 		Node *node = list->head;
-
+		
+		//While the node is valid.
 		while(node != NULL) {
 			Document *doc = (Document *)node->data;
 			scores[doc->id] += doc->score;
+			// Move onto the next node.
 			node = node->next;
 		}
 	} // O(dt)
 
+	// Store the top k in a priority queue.
 	heap_t *h = new_heap(n_results);
 
 	for(int i=0; i < n_documents; i++) {
+		// We haven't filled the heap yet.
 		if(i < n_results) {
 			heap_insert(h, scores[i], i); 
+		// Heap is full
 		}else {
+			//Extract the minimum key val
 			float min_val = heap_peak_key(h);
+			// If the current value is bigger than
+			// the smallest value in our min heap.
 			if(scores[i] > min_val) {
+				// The smallest in the heap
+				// is no longer among the top k.
 				heap_remove_min(h);
+				// The new value belongs in the top k.
 				heap_insert(h, scores[i], i);  
 			}
 		}
 	} 
 
+	//Need to store the ids.
 	int *ids = malloc(n_results*sizeof(int));
 
 	for(int i=0; i < n_results; i++) {
@@ -68,6 +83,6 @@ void print_array_results(Index *index, int n_results, int n_documents) {
 // see Assignment spec: 'Task 2: Priority queue-based multi-way merge approach'
 void print_merge_results(Index *index, int n_results) {
 
-	printf("not yet implemented: put code for task 2 here\n");
+	
 
 }
