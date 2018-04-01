e9c9233d9abedbc5a8b4afe9bfe3c821be9b8328
isubasinghe <isubasinghe@student.unimelb.edu.au>
2018-04-01 12:18:15 +1000

    Added comments and apparently n_results < n_documents were a valid case. So fixed that.

--- query.c
+++ query.c
@@ -1,7 +1,8 @@
 /* ... */
 
-#define TEST_SUITE_ON
+//#define TEST_SUITE_ON
 
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,7 +33,6 @@ void write_csv(int task, int n_results, int num_terms,
 
 // see Assignment spec: 'Task 1: Array-based accumulation approach'
 void print_array_results(Index *index, int n_results, int n_documents) {
-	
 	#ifdef TEST_SUITE_ON
 	clock_t start_t, end_t;
 	float total=0.0;
@@ -41,6 +41,7 @@ void print_array_results(Index *index, int n_results, int n_documents) {
 	
 	#endif // TEST_SUITE_ON
 
+
 	// Allocate n_documents scores
 	float *scores = malloc(n_documents*sizeof(float));
 	assert(scores != NULL);
@@ -48,7 +49,7 @@ void print_array_results(Index *index, int n_results, int n_documents) {
 	for(int i=0; i < n_documents; i++) {
 		// We need to assure that all the elems are 0.
 		scores[i] = 0.0;
-	} // O(d)
+	} 
 
 
 	for(int i=0; i < index->num_terms; i++) {
@@ -67,56 +68,43 @@ void print_array_results(Index *index, int n_results, int n_documents) {
 			// Move onto the next node.
 			node = node->next;
 		}
-	} // O(dt)
-
-	// Store the top k in a priority queue.
-	heap_t *h = new_heap(n_results);
-	assert(h != NULL);
+	} 
+	
+	heap_t *topk = new_heap(n_results);
 
 	for(int i=0; i < n_documents; i++) {
-		// We haven't filled the heap yet.
-		if(i < n_results) {
-			heap_insert(h, scores[i], i); 
-		// Heap is full
+		//We can insert, our heap isn't full yet.
+		if(topk->len < n_results) {
+			heap_insert(topk, scores[i], i);
 		}else {
-			//Extract the minimum key val
-			float min_val = heap_peak_key(h);
-			// If the current value is bigger than
-			// the smallest value in our min heap.
-			if(scores[i] > min_val) {
-				// The smallest in the heap
-				// is no longer among the top k.
-				heap_remove_min(h);
-				// The new value belongs in the top k.
-				heap_insert(h, scores[i], i);  
+			float min = heap_peak_key(topk);
+			// check i fthe smallest value in the heap
+			// is greater than the current score.
+			if(scores[i] > min) {
+				// swap the scores, since the current is bigger.
+				heap_remove_min(topk);
+				heap_insert(topk, scores[i], i);
 			}
 		}
-	} 
+	}
 
-	//Need to store the ids.
+	//Store the ids in this.
 	int *ids = malloc(n_results*sizeof(int));
-	assert(ids != NULL);
+	assert(ids!=NULL);
+
 
 	for(int i=0; i < n_results; i++) {
-		float score = heap_peak_key(h);
-		int id = heap_remove_min(h);
-		// Re-use the score array to save space.
-		scores[n_results-1-i] = score;
-		ids[i] = id;
+		//Save a little bit of memory and cut overhead,
+		// by using the scores array.
+		scores[n_results-1-i] = heap_peak_key(topk);
+		ids[n_results-1-i] = heap_remove_min(topk);
 	}
-	free_heap(h);
 
 	for(int i=0; i < n_results; i++) {
-		#ifndef TEST_SUITE_ON
-		//printf("%6d %.6f\n", ids[i], scores[i]);
 		printf("%6d %.6f\n", ids[i], scores[i]);
-		#endif // TEST_SUITE_ON
 	}
 
-	free(ids);
-	free(scores);
-
-
+	free_heap(topk);
 	
 	#ifdef TEST_SUITE_ON
 	end_t = clock();
@@ -140,12 +128,14 @@ void print_merge_results(Index *index, int n_results) {
 	start_t = clock();
 	#endif // TEST_SUITE_ON
 	
+	// Store the top n results in a heap.
 	heap_t *topk = new_heap(n_results);
 	assert(topk!=NULL);
 
 	Node **nodes = malloc(index->num_terms *sizeof(Node *));
 	assert(nodes!=NULL);
 
+	// Intialise the nodes array.
 	for(int i=0; i<index->num_terms; i++) {
 		nodes[i] = index->doclists[i]->head;
 	}
@@ -155,39 +145,46 @@ void print_merge_results(Index *index, int n_results) {
 		heap_t *index_heap = new_heap(index->num_terms);
 		assert(index_heap!=NULL);
 
+		//Count the number of null nodes.
 		int null_c = 0;
 		for(int i=0; i < index->num_terms; i++) {
 			if(nodes[i] == NULL) {
 				null_c++;
-				continue;
+				continue; // We cant process null nodes.
 			}else {
 				Document *doc = (Document *)nodes[i]->data;
 				heap_insert(index_heap, doc->id, i);
 			}
 			
 		}
-
+		// If the number of null nodes >= number of terms, 
+		// we have gone through all the doclists till they are empty.
+		// This is our exit case.
 		if(null_c >= index->num_terms) break;
 
 		int id = heap_peak_key(index_heap);
-		float score = 0.0;
+		float score = 0.0; // Initialise a score to zero.
 		while(index_heap->len > 0) {
 			int temp_id = heap_peak_key(index_heap);
+			// Check to see if we are operating on the same doc id.
 			if(temp_id == id) {
 				int i = heap_remove_min(index_heap);
 				Document *doc = (Document *)nodes[i]->data;
 				score += doc->score;
+				// this node has been dealt with, move on to the next.
 				nodes[i] = nodes[i]->next;
 			}else {
 				break;
 			}
 		}
-
+		// Insert to our heap if the heap is empty.
 		if(topk->len < n_results) {
 			heap_insert(topk, score, id);
 		}else {
+			// Heap is not empty, so we check if the current value is bigger.
 			float min = heap_peak_key(topk);
 			if(min < score) {
+				//Swap the current value with the minimum on the heap.
 				heap_remove_min(topk);
 				heap_insert(topk, score, id);
 			}
@@ -195,11 +192,13 @@ void print_merge_results(Index *index, int n_results) {
 
 		free_heap(index_heap);
 	}
+	// Store our scores and ids in an array.
 	float *scores = malloc(n_results*sizeof(float));
 	assert(scores!=NULL);
 	int *ids = malloc(n_results*sizeof(int));
 	assert(ids!=NULL);
 
+	//Insert in reverse order to the scores and ids array.
 	for(int i=0; i < n_results; i++) {
 		scores[n_results-1-i] = heap_peak_key(topk);
 		ids[n_results-1-i] = heap_remove_min(topk);
@@ -207,6 +206,8 @@ void print_merge_results(Index *index, int n_results) {
 
 	for(int i=0; i<n_results; i++) {
 		#ifndef TEST_SUITE_ON
+		// These are non valid, a result of n_results < n_documents.
+		if(ids[i]==-1) continue;
 		printf("%6d %0.6f\n", ids[i], scores[i]);
 		#endif //TEST_SUITE_ON
 	}
